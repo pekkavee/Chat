cd17d1a3e23aa11983a2439e4324f92c226b8002
Pekka <xpekka.venalainen@capgemini.com>
2018-03-09 15:19:17 +0200

    chat

---
+++ src/mychatapp/Chat.java
@@ -0,0 +1,19 @@
+
+package mychatapp;
+
+import mychatapp.gui.FirstScreen;
+import mychatapp.gui.MainScreen;
+
+
+public class Chat {
+
+    public static void main(String[] args) {
+       
+     FirstScreen fs =new FirstScreen();
+    
+    
+        
+        
+    }
+    
+}
